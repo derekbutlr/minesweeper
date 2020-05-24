218a9400395f1f8b44ed9b5ec18bccd095752094
Derek <levidvi@gmail.com>
2020-05-24 21:09:27 +1200

    minesweeper works

--- minesweeper.js
+++ minesweeper.js
@@ -1,39 +1,62 @@
 document.addEventListener('DOMContentLoaded', startGame)
-
 // Define your `board` object here!
 var board = {
-
+    cells: [
+        {
+            row: 0,
+            col: 0,
+            isMine: true,
+            isMarked: false,
+            hidden: true,
+            surroundingMines: 0
+        },
+        {
+            row: 0,
+            col: 1,
+            isMine: false,
+            isMarked: false,
+            hidden: true,
+            surroundingMines: 0
+        },
+        {
+            row: 1,
+            col: 0,
+            isMine: false,
+            isMarked: false,
+            hidden: true,
+            surroundingMines: 0
+        },
+        {
+            row: 1,
+            col: 1,
+            isMine: false,
+            isMarked: false,
+            hidden: true,
+            surroundingMines: 0
+        }
+    ]
 }
-
-board.cells = [
-  {row: 0, col: 0, isMine:false, hidden:true},
-  {row: 0, col: 1, isMine:true, hidden:true},
-  {row: 0, col: 2, isMine:true, hidden:true},
-  {row: 1, col: 0, isMine:true, hidden:true},
-  {row: 1, col: 1, isMine:false, hidden:true},
-  {row: 1, col: 2, isMine:true, hidden:true},
-  {row: 2, col: 0, isMine:true, hidden:true},
-  {row: 2, col: 2, isMine:true, hidden:true},
-  {row: 2, col: 1, isMine:true, hidden:true},
-]
-
-
-function startGame () {
-  // Don't remove this function call: it makes the game work!
-  lib.initBoard()
+function startGame() {
+    for (var i = 0; i < board.cells.length; i++) {
+        var cell = board.cells[i]
+        cell.surroundingMines = countSurroundingMines(cell)
+    }
+    lib.initBoard()
 }
-
-// Define this function to look for a win condition:
-//
-// 1. Are all of the cells that are NOT mines visible?
-// 2. Are all of the mines marked?
+document.addEventListener("click", checkForWin)
+document.addEventListener("contextmenu", checkForWin)
 function checkForWin () {
-
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+    var imTheWinner = true
+    for (var i = 0; i < board.cells.length; i++) {
+        var cell = board.cells[i]
+        if (cell.isMine == true && cell.isMarked == false) {
+            imTheWinner = false
+        }
+    }
+    if (imTheWinner) {
+        alert("I'm the winner")
+    }
 }
-
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
 // cells yourself! Just use `lib.getSurroundingCells`: 
@@ -42,8 +65,15 @@ function checkForWin () {
 //
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
-function countSurroundingMines (cell) {
+
+function countSurroundingMines(cell) {
+    var count = 0
+    var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+    for (var i = 0; i < surrounding.length; i++) {
+        var surroundingCell = surrounding[i]
+        if (surroundingCell.isMine == true) {
+            count++
+        }
+    }
+    return count
 }
-
-
-
